d9ec4398b02f80d9f3c22fb275c4204c45f00037
Jeff Lawson <jeff@bovine.net>
2010-10-29 01:06:00 -0500

    use package require instead

--- tests/all.tcl
+++ tests/all.tcl
@@ -1,7 +1,14 @@
 
-load ./memcache[info sharedlibextension]
+package require Memcache
+#load ./memcache[info sharedlibextension]
 
 memcache server add localhost 11211
 memcache set moo "cows go moo"
 memcache get moo value
-puts "value=$value!\n";
+if {$value != "cows go moo"} {
+    puts "Error.  value=$value!\n";
+    exit 1
+}
+
+puts "Success"
+exit 0
