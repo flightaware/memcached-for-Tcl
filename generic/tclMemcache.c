b45834ff47fd1e9a0b0f1b552e6424a3b76c662e
Jeff Lawson <jeff.lawson@flightaware.com>
2016-05-17 17:44:26 +0000

    add new "behavior" command for adjusting libmemcached settings.

--- generic/tclMemcache.c
+++ generic/tclMemcache.c
@@ -56,12 +56,56 @@ static int Memcache_Cmd(ClientData arg, Tcl_Interp * interp, int objc, Tcl_Obj *
 
   enum {
     cmdGet, cmdAdd, cmdAppend, cmdSet, cmdReplace,
-    cmdDelete, cmdIncr, cmdDecr, cmdVersion, cmdServer
+    cmdDelete, cmdIncr, cmdDecr, cmdVersion, cmdServer, cmdBehavior
   };
 
   static CONST char *sCmd[] = {
     "get", "add", "append", "set", "replace",
-    "delete", "incr", "decr", "version", "server",
+    "delete", "incr", "decr", "version", "server", "behavior",
+    0
+  };
+
+
+  // this list should match the order defined in include/libmemcached-1.0/types/behavior.h
+  static CONST char *sBehavior[] = {
+    "MEMCACHED_BEHAVIOR_NO_BLOCK",
+    "MEMCACHED_BEHAVIOR_TCP_NODELAY",
+    "MEMCACHED_BEHAVIOR_HASH",
+    "MEMCACHED_BEHAVIOR_KETAMA",
+    "MEMCACHED_BEHAVIOR_SOCKET_SEND_SIZE",
+    "MEMCACHED_BEHAVIOR_SOCKET_RECV_SIZE",
+    "MEMCACHED_BEHAVIOR_CACHE_LOOKUPS",
+    "MEMCACHED_BEHAVIOR_SUPPORT_CAS",
+    "MEMCACHED_BEHAVIOR_POLL_TIMEOUT",
+    "MEMCACHED_BEHAVIOR_DISTRIBUTION",
+    "MEMCACHED_BEHAVIOR_BUFFER_REQUESTS",
+    "MEMCACHED_BEHAVIOR_USER_DATA",
+    "MEMCACHED_BEHAVIOR_SORT_HOSTS",
+    "MEMCACHED_BEHAVIOR_VERIFY_KEY",
+    "MEMCACHED_BEHAVIOR_CONNECT_TIMEOUT",
+    "MEMCACHED_BEHAVIOR_RETRY_TIMEOUT",
+    "MEMCACHED_BEHAVIOR_KETAMA_WEIGHTED",
+    "MEMCACHED_BEHAVIOR_KETAMA_HASH",
+    "MEMCACHED_BEHAVIOR_BINARY_PROTOCOL",
+    "MEMCACHED_BEHAVIOR_SND_TIMEOUT",
+    "MEMCACHED_BEHAVIOR_RCV_TIMEOUT",
+    "MEMCACHED_BEHAVIOR_SERVER_FAILURE_LIMIT",
+    "MEMCACHED_BEHAVIOR_IO_MSG_WATERMARK",
+    "MEMCACHED_BEHAVIOR_IO_BYTES_WATERMARK",
+    "MEMCACHED_BEHAVIOR_IO_KEY_PREFETCH",
+    "MEMCACHED_BEHAVIOR_HASH_WITH_PREFIX_KEY",
+    "MEMCACHED_BEHAVIOR_NOREPLY",
+    "MEMCACHED_BEHAVIOR_USE_UDP",
+    "MEMCACHED_BEHAVIOR_AUTO_EJECT_HOSTS",
+    "MEMCACHED_BEHAVIOR_NUMBER_OF_REPLICAS",
+    "MEMCACHED_BEHAVIOR_RANDOMIZE_REPLICA_READ",
+    "MEMCACHED_BEHAVIOR_CORK",
+    "MEMCACHED_BEHAVIOR_TCP_KEEPALIVE",
+    "MEMCACHED_BEHAVIOR_TCP_KEEPIDLE",
+    "MEMCACHED_BEHAVIOR_LOAD_FROM_FILE",
+    "MEMCACHED_BEHAVIOR_REMOVE_FAILED_SERVERS",
+    "MEMCACHED_BEHAVIOR_DEAD_TIMEOUT",
+    "MEMCACHED_BEHAVIOR_SERVER_TIMEOUT_LIMIT",
     0
   };
 
@@ -185,6 +229,22 @@ static int Memcache_Cmd(ClientData arg, Tcl_Interp * interp, int objc, Tcl_Obj *
     Tcl_SetObjResult(interp, Tcl_ObjPrintf("%s (with libmemcached %s)", PACKAGE_STRING, memcached_lib_version()));
     break;
 
+
+  case cmdBehavior:
+    if (objc < 3 || objc > 4) {
+      Tcl_WrongNumArgs(interp, 2, objv, "flagname ?flagvalue?");
+      return TCL_ERROR;
+    }
+    if (Tcl_GetIndexFromObj(interp, objv[2], sBehavior, "command", TCL_EXACT, (int *) &cmd) != TCL_OK) {
+      return TCL_ERROR;
+    }
+    if (objc > 3) {
+      result = memcached_behavior_set(get_memc(), cmd, atoll(Tcl_GetString(objv[3])));
+      Tcl_SetObjResult(interp, Tcl_NewIntObj(result));
+    } else {
+      uint64_t currentVal = memcached_behavior_get(get_memc(), cmd);
+      Tcl_SetObjResult(interp, Tcl_NewWideIntObj(currentVal));
+    }
   }
   return TCL_OK;
 }
